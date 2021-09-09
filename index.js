61be5407973473487542cf26c60efd02dbf35f34
Moniquevb <64192648+Moniquevb@users.noreply.github.com>
2021-09-09 13:44:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+let myName = "Monique"
+console.log(myName);
+
+let numbers = 4 + 4
+console.log(numbers);
+let string = "4" + "4"
+console.log(string);
+
+console.log(2 + 2);
+console.log(6 - 3);
+console.log(5 * 7);
+console.log(9 / 4);
+console.log(10 % 3);
