8bd94dad995a2a10590665d5124854a02799e523
Moniquevb <64192648+Moniquevb@users.noreply.github.com>
2021-09-09 16:49:30 +0200

    Add files via upload
    objects & arrays

---
+++ objects.js
@@ -0,0 +1,24 @@
+let person = {
+    name: 'Monique',
+    age: 32,
+    evaluations: [7, 10, 9]
+}
+
+console.log(person);
+console.log(person['name']);
+console.log(person.age);
+console.log(person.evaluations);
+
+let kleuren = ['groen', 'blauw', 'rood']
+console.log(kleuren);
+console.log(kleuren.length);
+console.log(kleuren[0]);
+console.log(kleuren[kleuren.length - 1]);
+kleuren.push('geel');
+console.log(kleuren);
+kleuren.push(5);
+console.log(kleuren);
+object = { greeting: "hi ik ben een object" }
+kleuren.push(object);
+console.log(kleuren);
+console.log(kleuren[kleuren.length - 1]);
