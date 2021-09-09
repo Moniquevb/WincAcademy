c8d3a4e3022edf61860766102f9ee9771d170610
Moniquevb <64192648+Moniquevb@users.noreply.github.com>
2021-09-09 14:28:58 +0200

    Add files via upload
    practice if/else statements

---
+++ script2.js
@@ -0,0 +1,23 @@
+const age = 18;
+
+if (age >= 18) {
+    console.log("Je mag naar binnen");
+} else {
+    console.log("Je mag nog geen alcohol. Helaas kan ik je niet binnen laten.")
+}
+
+const isFemale = false;
+
+if (isFemale) {
+    console.log("Je bent welkom op de ladiesnight!");
+} else {
+    console.log("Je bent geen vrouw. Helaas ben je niet welkom op de ladiesnight");
+}
+
+const driverStatus = "tessa";
+
+if (driverStatus === "bob") {
+    console.log("Je bent de bob vanavond. Je mag rijden.");
+} else {
+    console.log("Vanavond ben je niet de bob. Waarschijnlijk heb je teveel gedronken en dus mag je niet rijden.")
+}
