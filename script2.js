b49b049f8bb6528accde927abd8b9620dd3f03cf
Moniquevb <64192648+Moniquevb@users.noreply.github.com>
2021-09-09 15:36:42 +0200

    Add files via upload
    comparisons && logical operators

--- script2.js
+++ script2.js
@@ -1,9 +1,15 @@
-const age = 18;
+const age = 26;
 
 if (age >= 18) {
     console.log("Je mag naar binnen");
 } else {
     console.log("Je mag nog geen alcohol. Helaas kan ik je niet binnen laten.")
+}
+
+if (age >= 18 && age < 26) {
+    console.log("Je krijgt 50% korting");
+} else {
+    console.log("Je krijgt geen korting");
 }
 
 const isFemale = false;
@@ -20,4 +26,24 @@ if (driverStatus === "bob") {
     console.log("Je bent de bob vanavond. Je mag rijden.");
 } else {
     console.log("Vanavond ben je niet de bob. Waarschijnlijk heb je teveel gedronken en dus mag je niet rijden.")
+}
+
+const firstName = "Sarah";
+
+if (firstName === "Sarah" || firstName === "Bram") {
+    console.log("Je krijgt een gratis biertje");
+} else {
+    console.log("Sorry, je krijgt geen gratis biertje");
+}
+
+const totalAmount = 30;
+
+if (totalAmount > 25) {
+    console.log("Gratis vegabitterballen");
+} else if (totalAmount > 50) {
+    console.log("Gatis nacho's");
+} else if (totalAmount > 100) {
+    console.log("Gratis champagne");
+} else {
+    console.log("Besteedt meer dan 25 euro en je krijgt gratis snacks!");
 }
