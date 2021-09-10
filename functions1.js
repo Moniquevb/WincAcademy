12fcefcdb7c308a54be1118b14332dd4333afc65
Moniquevb <64192648+Moniquevb@users.noreply.github.com>
2021-09-10 13:07:55 +0200

    Add files via upload
    functions basics

---
+++ functions1.js
@@ -0,0 +1,22 @@
+const makePizza = function () {
+    console.log("combine flour, water & yeast");
+    console.log("kneed ingredients into dough");
+    console.log("roll out the dough");
+    console.log("top up it with sauce & cheese");
+    console.log("put in the oven for 15 minutes");
+}
+
+const makeSushi = function () {
+    console.log("boil rice");
+    console.log("chop cucumber & avocado & salmon");
+    console.log("place a layer of rice onto a nori sheet");
+    console.log("place cucumber, avocado, salmon in the middle");
+    console.log("roll the nori, rice & other ingredients into a roll");
+    console.log("cut the roll into maki sushi");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
