f5de5df877423a5e016e5f0a7d2f535c785070a8
Monique <moniquevanbenthem@gmail.com>
2021-09-15 11:24:43 +0200

    for loops

---
+++ Javascript/loops/multiplication-tables.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 10; i++) {
+    //const tableresult = 9 * i;
+    //console.log(tableresult);
+    for (let x = 1; x <=10; x++) {
+    const tableresult = x * i;
+    console.log(`${i} x ${x} =`, tableresult);
+    }
+}
