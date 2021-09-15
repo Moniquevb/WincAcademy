8c7f32e190d387157b98b514134b80da551e5c87
Monique <moniquevanbenthem@gmail.com>
2021-09-15 12:12:02 +0200

    for & while  loops & arrays

---
+++ Javascript/loops2/colors.js
@@ -0,0 +1,15 @@
+/* colors = ['yellow', 'blue', 'red', 'orange'];
+let i = 0;
+while (colors[i]) {
+    console.log(colors[i]);
+    i++;
+}
+ */
+const colors = ["yellow", "blue", "red", "orange"];
+for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+const array1 = ["yellow", "blue", "red", "orange"];
+
+array1.forEach(element => console.log(element));
