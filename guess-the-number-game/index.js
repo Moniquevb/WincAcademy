ad96e6c374b66810869902eb34d3bfa2e519fd45
Moniquevb <64192648+Moniquevb@users.noreply.github.com>
2021-09-17 10:46:08 +0200

    Update index.js

---
+++ guess-the-number-game/index.js
@@ -0,0 +1,11 @@
+//When the user clicks enter
+//Then I want to see a message "Hello name"
+// get element name
+// get element submit button
+// get elemement message 
+
+//And ik wil gevraagd worden om een nieuwe input met een cijfer. e.g: "Voer een nummer in van 0 tot 25 om te beginnen met raden..."
+//Als spelmaker wil ik dat de gebruiker wel een reeële kans heeft om te winnen. Daarom pak ik een willekeurig getal van 0 tot 25. Hint: gebruikt Math.random()
+// Als gebruiker wil ik een bericht zien als ik het nummer verkeerd heb geraden e.g. "Dat is niet correct" . Daarna wil ik gevraagd worden om opnieuw te raden.
+// Als gebruiker wil ik een bericht ontvangen als ik het nummer goed heb geraden. e.g: "Gefeliciteerd je hebt gewonnen". Het spel is nu af.
+// Als gebruiker wil ik een bericht zien als het spel af is. "e.g: Dag [naam]. Tot de volgende keer"
