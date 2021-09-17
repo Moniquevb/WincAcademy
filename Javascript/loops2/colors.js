37580957a73b9e87465573517c2458167e18527d
Monique <moniquevanbenthem@gmail.com>
2021-09-17 10:43:10 +0200

    guess the number game

--- Javascript/loops2/colors.js
+++ Javascript/loops2/colors.js
@@ -1,13 +1,14 @@
-/* colors = ['yellow', 'blue', 'red', 'orange'];
-let i = 0;
-while (colors[i]) {
-    console.log(colors[i]);
-    i++;
+colors1 = ['yellow', 'blue', 'red', 'orange'];
+let a = 0;
+while (colors1[a]) {
+    console.log(colors1[a]);
+    a++;
 }
- */
-const colors = ["yellow", "blue", "red", "orange"];
-for (var i = 0; i < colors.length; i++) {
-    console.log(colors[i]);
+
+
+const colors2 = ["yellow", "blue", "red", "orange"];
+for (var i = 0; i < colors2.length; i++) {
+    console.log(colors2[i]);
 }
 
 const array1 = ["yellow", "blue", "red", "orange"];
