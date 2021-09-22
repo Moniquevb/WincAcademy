58b7be3bae4804d5f50f6a8ae9df82d9fb6b4f7d
Moniquevb <64192648+Moniquevb@users.noreply.github.com>
2021-09-22 11:43:33 +0200

    Update script.js

--- Eindopdracht/script.js
+++ Eindopdracht/script.js
@@ -1,37 +1,40 @@
 const menuIcon = document.querySelector("#menu-link");
 const menu = document.querySelector(".menu");
-const bgColorYellow = document.querySelector("#bg-color-yellow");
-const bgColorRed = document.querySelector("#bg-color-red");
-const bgColorGreen = document.querySelector("#bg-color-green");
-const bgColorBlue = document.querySelector("#bg-color-blue");
-const bgColorPurple = document.querySelector("#bg-color-purple");
+const bgColorYellow = document.querySelector(".bg-color-yellow");
+const bgColorRed = document.querySelector(".bg-color-red");
+const bgColorGreen = document.querySelector(".bg-color-green");
+const bgColorBlue = document.querySelector(".bg-color-blue");
+const bgColorPurple = document.querySelector(".bg-color-purple");
 const changeBgColor = document.querySelector("#change-bg-color");
 const bgColorClass = document.querySelector(".bg-color-white");
 
 bgColorYellow.addEventListener('click', function () {
     bgColorClass.className = "bg-color-yellow";
+    menu.classList.toggle("menu2");
 })
 
 bgColorGreen.addEventListener('click', function () {
     bgColorClass.className = "bg-color-green";
+    menu.classList.toggle("menu2");
 })
 
 bgColorRed.addEventListener('click', function () {
     bgColorClass.className = "bg-color-red";
+    menu.classList.toggle("menu2");
 })
 
 bgColorBlue.addEventListener('click', function () {
     bgColorClass.className = "bg-color-blue";
+    menu.classList.toggle("menu2")
 })
 
 bgColorPurple.addEventListener('click', function () {
     bgColorClass.className = "bg-color-purple";
+    menu.classList.toggle("menu2")
 })
 
 menuIcon.addEventListener('click', function () {
     menu.classList.toggle("menu2")
 })
 
-// menuIcon.addEventListener('click', function () {
-//     menu.style.visibility = "visible";
-// })
+
